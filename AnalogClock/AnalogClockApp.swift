07439ae654e66d04fbd3f06eebfe9dd81e6f67f0
Mohammed Shafeer <shafeer@digivalsolutions.com>
2022-05-25 09:04:00 +0530

    Initial Commit

---
+++ AnalogClock/AnalogClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalogClockApp.swift
+//  AnalogClock
+//
+//  Created by MohammedShafeer on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct AnalogClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
